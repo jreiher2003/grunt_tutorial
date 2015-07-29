6978ceeef50cb1bb045fdf7539f716899074854a
Jeff Reiher <jeffreiher@gmail.com>
2015-07-29 12:47:06 -0400

    step 6 after downloading npm package configure Gruntfile.js to use package

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,13 +8,26 @@ module.exports = function(grunt) {
         src: ['js/libs/*.js','js/app.js'], // input 
         dest: 'js/build/app.min.js'  // output
       }
+    },
+    sass: {
+       options: {
+            sourceMap: true
+        },
+        dist: {
+            files: {
+                'main.css': 'main.scss'
+            }
+        }
+
+
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['uglify','sass']);
 
 };
